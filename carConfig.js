56dc5b88a9583bfd19837277e7da2a3de6e395f0
njanman <45427738+njanman@users.noreply.github.com>
2018-12-13 19:45:16 +0000

    Add files via upload

--- carConfig.js
+++ carConfig.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     $("#carouselHeadlights").attr("src", "RS5/rs5BHeadlights.jpeg");
     $("#carouselAlloys").attr("src", "RS5/rs5BAlloys.jpeg");
   });
+
   $("#grey").click(function(){
     $("#mainColor").attr("src", "RS5/rs5FrontSide.jpeg");
     $("#final").attr("src", "RS5/rs5Side.jpeg");
@@ -35,6 +36,7 @@ $(document).ready(function(){
     $("#carouselHeadlights").attr("src", "RS5/rs5BHeadlights.jpeg");
     $("#carouselAlloys").attr("src", "RS5/rs5BAlloys.jpeg");
   });
+
   $("#20").click(function(){
     $("#alloys").attr("src", "RS5/rs520Alloys.jpeg");
     $("#final").attr("src", "RS5/rs5Side20Alloy.jpeg");
@@ -46,17 +48,60 @@ $(document).ready(function(){
     $("#carouselHeadlights").attr("src", "RS5/rs5Headlights.jpeg");
     $("#carouselAlloys").attr("src", "RS5/rs5Alloys.jpeg");
   });
-  var alloy = $("#alloys");
-  var main = $("#mainColor");
-  var blue = $("src", "RS5/rs5BFrontSide.jpeg");
-  var grey = $("src", "RS5/rs5FrontSide.jpeg");
-  var alloy20Blue = $("src", "RS5/rs5B20Alloys.jpeg");
-  var alloyBlue = $("src", "RS5/rs5BAlloys");
-  var alloy20 = $("src", "RS5/rs520Alloys.jpeg");
+
+  $("#LED").click(function(){
+    $("#headlights").attr("src", "RS5/rs5LED.jpeg");
+    /*$("#final").attr("src", "RS5/rs5Side20Alloy.jpeg");
+    $("#carouselFrontSide").attr("src", "RS5/rs5FrontSide.jpeg");
+    $("#carouselSide").attr("src", "RS5/rs5Side.jpeg");
+    $("#carouselRearSide").attr("src", "RS5/rs5RearSide.jpeg");
+    $("#carouselRear").attr("src", "RS5/rs5Rear.jpeg");
+    $("#carouselFront").attr("src", "RS5/rs5Front.jpeg");
+    $("#carouselHeadlights").attr("src", "RS5/rs5Headlights.jpeg");
+    $("#carouselAlloys").attr("src", "RS5/rs5Alloys.jpeg");*/
+  });
+
+  $("#matrix").click(function(){
+    $("#headlights").attr("src", "RS5/rs5Matrix.jpeg");
+    /*$("#final").attr("src", "RS5/rs5Side20Alloy.jpeg");
+    $("#carouselFrontSide").attr("src", "RS5/rs5FrontSide.jpeg");
+    $("#carouselSide").attr("src", "RS5/rs5Side.jpeg");
+    $("#carouselRearSide").attr("src", "RS5/rs5RearSide.jpeg");
+    $("#carouselRear").attr("src", "RS5/rs5Rear.jpeg");
+    $("#carouselFront").attr("src", "RS5/rs5Front.jpeg");
+    $("#carouselHeadlights").attr("src", "RS5/rs5Headlights.jpeg");
+    $("#carouselAlloys").attr("src", "RS5/rs5Alloys.jpeg");*/
+  });
+
+  $("#change").click(function(){
+
+    var alloy       = $("#alloys");
+    var main        = $("#mainColor");
+    var blueColor   = $("src", "RS5/rs5BFrontSide.jpeg");
+    var grey        = $("src", "RS5/rs5FrontSide.jpeg");
+    var alloy20Blue = $("src", "RS5/rs5B20Alloys.jpeg");
+    var alloyBlue   = $("src", "RS5/rs5BAlloys.jpeg");
+    var alloy20     = $("src", "RS5/rs520Alloys.jpeg");
+
+    localStorage.setItem("alloys", alloy);
+    localStorage.setItem("mainColor", main);
+    localStorage.setItem("RS5/rs5BFrontSide.jpeg", blueColor);
+    localStorage.setItem("RS5/rs5FrontSide.jpeg", grey);
+    localStorage.setItem("RS5/rs5B20Alloys.jpeg", alloy20Blue);
+    localStorage.setItem("RS5/rs5BAlloys.jpeg", alloyBlue);
+    localStorage.setItem("RS5/rs520Alloys.jpeg", alloy20);
+
+    console.log(main);
+  })
+
+
+
+
   
-  if (main == blue && alloy == alloy20Blue){
-    $("#final").attr("src", "RS5/rs5BSide20Alloy.jpeg");
-  }else  if (main == grey && alloy == alloy20){
-    $("#final").attr("src", "RS5/rs5Side20Alloy.jpeg");
-  }
+  
+  //if (main == blue && alloy == alloy20Blue){
+    //$("#final").attr("src", "RS5/rs5BSide20Alloy.jpeg");
+  //}else  if (main == grey && alloy == alloy20){
+    //$("#final").attr("src", "RS5/rs5Side20Alloy.jpeg");
+  //}
 })
